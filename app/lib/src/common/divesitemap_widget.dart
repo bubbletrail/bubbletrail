b140f14f5659bd70cbe6796a2df1f285d4d36a1a
Jakob Borg <jakob@kastelo.net>
2025-12-14 15:39:47 +0100

    recenter map when dive changes

--- app/lib/src/common/divesitemap_widget.dart
+++ app/lib/src/common/divesitemap_widget.dart
@@ -4,15 +4,31 @@ import 'package:latlong2/latlong.dart';
 
 import '../ssrf/ssrf.dart';
 
-class DiveSiteMap extends StatelessWidget {
+class DiveSiteMap extends StatefulWidget {
   const DiveSiteMap({super.key, required this.position});
 
   final GPSPosition position;
 
   @override
+  State<DiveSiteMap> createState() => _DiveSiteMapState();
+}
+
+class _DiveSiteMapState extends State<DiveSiteMap> {
+  final MapController _mapController = MapController();
+
+  @override
+  void didUpdateWidget(DiveSiteMap oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.position.lat != widget.position.lat || oldWidget.position.lon != widget.position.lon) {
+      _mapController.move(LatLng(widget.position.lat, widget.position.lon), _mapController.camera.zoom);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final latlng = LatLng(position.lat, position.lon);
+    final latlng = LatLng(widget.position.lat, widget.position.lon);
     return FlutterMap(
+      mapController: _mapController,
       options: MapOptions(initialCenter: latlng, initialZoom: 15.0, minZoom: 3.0, maxZoom: 18.0),
       children: [
         TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', userAgentPackageName: 'org.divepath.divepath', maxZoom: 19),
