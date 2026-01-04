dfae2b446206483cfb458eb2109e4625997818a0
Jakob Borg <jakob@kastelo.net>
2026-01-04 18:53:45 +0100

    wip
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- app/build-macos-direct.sh
+++ app/build-macos-direct.sh
@@ -5,14 +5,15 @@ set -eou pipefail
 export APP_STORE_CONNECT_ISSUER_ID="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
 export APP_STORE_CONNECT_KEY_IDENTIFIER="$APP_STORE_CONNECT_API_KEY_KEY_ID"
 export APP_STORE_CONNECT_PRIVATE_KEY="$APP_STORE_CONNECT_API_KEY_KEY"
+tmp=$(mktemp -d)
 
-echo "$DEVELOPER_ID_APPLICATION_CERT" | base64 -d > developer-id-application-cert.p12
-openssl pkcs12 -in developer-id-application-cert.p12 -legacy -passin pass: -nodes -nocerts | openssl rsa -out developer-id-application-cert.key
+echo "$DEVELOPER_ID_APPLICATION_CERT" | base64 -d > "$tmp/developer-id-application-cert.p12"
+openssl pkcs12 -in "$tmp/developer-id-application-cert.p12" -legacy -passin pass: -nodes -nocerts | openssl rsa -out "$tmp/developer-id-application-cert.key"
 
 app-store-connect fetch-signing-files app.bubbletrail.bubbletrail \
     --platform MAC_OS \
     --type MAC_APP_DIRECT \
-    --certificate-key=@file:developer-id-application-cert.key \
+    --certificate-key=@file:"$tmp/developer-id-application-cert.key" \
     --create
 
 keychain initialize
@@ -32,12 +33,14 @@ flutter build macos \
 
 APP_NAME=$(find build/macos -name "*.app")
 ZIP_NAME=$(basename "$APP_NAME" .app).zip
+
 mv "$APP_NAME" .
 zip -r "$ZIP_NAME" $(basename "$APP_NAME")
 
 # Notarize it
 
-echo "$APP_STORE_CONNECT_API_KEY_KEY" > api.key
-xcrun notarytool submit -k api.key -d "$APP_STORE_CONNECT_API_KEY_KEY_ID" -i "$APP_STORE_CONNECT_API_KEY_ISSUER_ID" "$ZIP_NAME"
+echo "$APP_STORE_CONNECT_API_KEY_KEY" > "$tmp/api.key"
+xcrun notarytool submit -k "$tmp/api.key" -d "$APP_STORE_CONNECT_API_KEY_KEY_ID" -i "$APP_STORE_CONNECT_API_KEY_ISSUER_ID" "$ZIP_NAME"
 
 keychain use-login
+rm -rf "$tmp"
