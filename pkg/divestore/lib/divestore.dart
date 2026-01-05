6c2a06502fa4c5453c8932eb4b3d2683fb073611
Jakob Borg <jakob@kastelo.net>
2026-01-05 10:02:16 +0100

    fix: properly map gas switches on DC import
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- pkg/divestore/lib/divestore.dart
+++ pkg/divestore/lib/divestore.dart
@@ -43,6 +43,8 @@ Dive convertDcDive(Log dl) {
     dive.start = dl.dateTime;
   }
 
+  final gasMixtoCylinderIdx = <int, int>{};
+
   // Convert tanks to cylinders
   for (var i = 0; i < dl.tanks.length; i++) {
     final tank = dl.tanks[i];
@@ -61,15 +63,34 @@ Dive convertDcDive(Log dl) {
       final gasMix = dl.gasMixes[tank.gasMixIndex];
       diveCylinder.oxygen = gasMix.oxygen;
       diveCylinder.helium = gasMix.helium;
+      gasMixtoCylinderIdx[tank.gasMixIndex] = i;
     }
 
     dive.cylinders.add(diveCylinder);
   }
 
-  // Collect events
+  // Make sure every gas mix has a tank
+  for (final (idx, gm) in dl.gasMixes.indexed) {
+    if (gasMixtoCylinderIdx.containsKey(idx)) continue;
+    final foundIdx = dive.cylinders.indexOf((dc) => dc.oxygen == gm.oxygen && dc.helium == gm.helium);
+    if (foundIdx >= 0) {
+      gasMixtoCylinderIdx[idx] = foundIdx;
+    } else {
+      dive.cylinders.add(DiveCylinder(oxygen: gm.oxygen, helium: gm.helium));
+      gasMixtoCylinderIdx[idx] = dive.cylinders.length - 1;
+    }
+  }
+
+  // Collect events, synthesize gas switch events
   for (final sample in dl.samples) {
     for (final event in sample.events) {
-      dive.events.add(event);
+      if (event.type != SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE) dive.events.add(event);
+    }
+    if (sample.hasGasMixIndex()) {
+      // Add a gas switch event. The value is the cylinder index.
+      dive.events.add(
+        SampleEvent(type: SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE, time: sample.time.toInt(), value: gasMixtoCylinderIdx[sample.gasMixIndex]),
+      );
     }
   }
 
