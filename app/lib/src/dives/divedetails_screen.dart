060f1174d1be10faf80aeaab88375ad7e36a1fed
Jakob Borg <jakob@kastelo.net>
2026-01-07 22:53:36 +0100

    wip
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- app/lib/src/dives/divedetails_screen.dart
+++ app/lib/src/dives/divedetails_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:divestore/divestore.dart';
-import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
