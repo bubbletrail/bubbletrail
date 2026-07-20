bb2654d267d8daaba79a3281e084f4e5db0c7ea4
Jakob Borg <jakob@kastelo.net>
2026-07-20 22:17:17 +0200

    chore: upgrade libdivecomputer, handle start/end location (#92)
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- pkg/libdivecomputer/lib/parser.dart
+++ pkg/libdivecomputer/lib/parser.dart
@@ -85,20 +85,14 @@ Log parseDiveFromParser(ffi.Pointer<dc.dc_parser_t> parser, {Uint8List? fingerpr
   // --- Salinity ---
   final salinityPtr = calloc<dc.dc_salinity_t>();
   if (_getField(parser, dc.dc_field_type_t.DC_FIELD_SALINITY, salinityPtr.cast())) {
-    final waterType = _convertWaterType(salinityPtr.ref.type);
-    if (waterType != null) {
-      log.salinity = Salinity(type: waterType, density: salinityPtr.ref.density);
-    }
+    log.salinity = Salinity(type: _convertWaterType(salinityPtr.ref.type), density: salinityPtr.ref.density);
   }
   calloc.free(salinityPtr);
 
   // --- Dive Mode ---
   final diveModePtr = calloc<ffi.UnsignedInt>();
   if (_getField(parser, dc.dc_field_type_t.DC_FIELD_DIVEMODE, diveModePtr.cast())) {
-    final mode = _convertDiveMode(diveModePtr.value);
-    if (mode != null) {
-      log.diveMode = mode;
-    }
+    log.diveMode = _convertDiveMode(dc.dc_divemode_t.fromValue(diveModePtr.value));
   }
   calloc.free(diveModePtr);
 
@@ -188,10 +182,17 @@ bool _getFieldIndexed(ffi.Pointer<dc.dc_parser_t> parser, dc.dc_field_type_t typ
 Log? _currentDive;
 LogSample? _currentSample;
 
+// Per-sample GPS location tracking. The first location sample becomes the
+// dive's start position and the last becomes the end position.
+Position? _firstSampleLocation;
+Position? _lastSampleLocation;
+
 /// Parse samples from the parser.
 void _parseSamples(ffi.Pointer<dc.dc_parser_t> parser, Log dive) {
   _currentDive = dive;
   _currentSample = null;
+  _firstSampleLocation = null;
+  _lastSampleLocation = null;
 
   final callback = ffi.NativeCallable<dc.dc_sample_callback_tFunction>.isolateLocal(_sampleCallback);
 
@@ -204,8 +205,19 @@ void _parseSamples(ffi.Pointer<dc.dc_parser_t> parser, Log dive) {
     dive.samples.add(_currentSample!);
   }
 
+  // Per-sample GPS location overrides the dive-level location: the first
+  // sample's location is the dive start, the last sample's the dive end.
+  if (_firstSampleLocation != null) {
+    dive.startPosition = _firstSampleLocation!;
+  }
+  if (_lastSampleLocation != null) {
+    dive.endPosition = _lastSampleLocation!;
+  }
+
   _currentDive = null;
   _currentSample = null;
+  _firstSampleLocation = null;
+  _lastSampleLocation = null;
 }
 
 /// Sample callback for dc_parser_samples_foreach.
@@ -233,7 +245,7 @@ void _sampleCallback(int typeValue, ffi.Pointer<dc.dc_sample_value_t> value, ffi
     case dc.dc_sample_type_t.DC_SAMPLE_EVENT:
       _currentSample?.events.add(
         SampleEvent(
-          type: _convertSampleEventType(value.ref.event.type),
+          type: _convertSampleEventType(dc.parser_sample_event_t.fromValue(value.ref.event.type)),
           time: value.ref.event.time,
           flags: value.ref.event.flags,
           value: value.ref.event.value,
@@ -271,7 +283,7 @@ void _sampleCallback(int typeValue, ffi.Pointer<dc.dc_sample_value_t> value, ffi
 
     case dc.dc_sample_type_t.DC_SAMPLE_DECO:
       _currentSample?.deco = DecoStatus(
-        type: _convertDecoStopType(value.ref.deco.type),
+        type: _convertDecoStopType(dc.dc_deco_type_t.fromValue(value.ref.deco.type)),
         time: value.ref.deco.time,
         depth: value.ref.deco.depth,
         tts: value.ref.deco.tts,
@@ -279,105 +291,104 @@ void _sampleCallback(int typeValue, ffi.Pointer<dc.dc_sample_value_t> value, ffi
 
     case dc.dc_sample_type_t.DC_SAMPLE_GASMIX:
       _currentSample?.gasMixIndex = value.ref.gasmix;
+
+    case dc.dc_sample_type_t.DC_SAMPLE_LOCATION:
+      final loc = value.ref.location;
+      final position = Position(latitude: loc.latitude, longitude: loc.longitude, altitude: loc.altitude != 0 ? loc.altitude : null);
+      _firstSampleLocation ??= position;
+      _lastSampleLocation = position;
   }
 }
 
 // --- Enum Conversion Functions ---
 
 /// Convert libdivecomputer water type to protobuf WaterType.
-WaterType? _convertWaterType(int dcValue) {
+WaterType _convertWaterType(dc.dc_water_t dcValue) {
   return switch (dcValue) {
-    0 => WaterType.WATER_TYPE_FRESH,
-    1 => WaterType.WATER_TYPE_SALT,
-    _ => null,
+    dc.dc_water_t.DC_WATER_FRESH => WaterType.WATER_TYPE_FRESH,
+    dc.dc_water_t.DC_WATER_SALT => WaterType.WATER_TYPE_SALT,
   };
 }
 
 /// Convert libdivecomputer dive mode to protobuf DiveMode.
-DiveMode? _convertDiveMode(int dcValue) {
+DiveMode _convertDiveMode(dc.dc_divemode_t dcValue) {
   return switch (dcValue) {
-    0 => DiveMode.DIVE_MODE_FREEDIVE,
-    1 => DiveMode.DIVE_MODE_GAUGE,
-    2 => DiveMode.DIVE_MODE_OPENCIRCUIT,
-    3 => DiveMode.DIVE_MODE_CLOSED_CIRCUIT_REBREATHER,
-    4 => DiveMode.DIVE_MODE_SEMI_CLOSED_REBREATHER,
-    _ => null,
+    dc.dc_divemode_t.DC_DIVEMODE_FREEDIVE => DiveMode.DIVE_MODE_FREEDIVE,
+    dc.dc_divemode_t.DC_DIVEMODE_GAUGE => DiveMode.DIVE_MODE_GAUGE,
+    dc.dc_divemode_t.DC_DIVEMODE_OC => DiveMode.DIVE_MODE_OPENCIRCUIT,
+    dc.dc_divemode_t.DC_DIVEMODE_CCR => DiveMode.DIVE_MODE_CLOSED_CIRCUIT_REBREATHER,
+    dc.dc_divemode_t.DC_DIVEMODE_SCR => DiveMode.DIVE_MODE_SEMI_CLOSED_REBREATHER,
   };
 }
 
 /// Convert libdivecomputer gas usage to protobuf GasUsage.
-GasUsage? _convertGasUsage(int dcValue) {
+GasUsage _convertGasUsage(dc.dc_usage_t dcValue) {
   return switch (dcValue) {
-    0 => GasUsage.GAS_USAGE_NONE,
-    1 => GasUsage.GAS_USAGE_OXYGEN,
-    2 => GasUsage.GAS_USAGE_DILUENT,
-    3 => GasUsage.GAS_USAGE_SIDEMOUNT,
-    _ => null,
+    dc.dc_usage_t.DC_USAGE_NONE => GasUsage.GAS_USAGE_NONE,
+    dc.dc_usage_t.DC_USAGE_OXYGEN => GasUsage.GAS_USAGE_OXYGEN,
+    dc.dc_usage_t.DC_USAGE_DILUENT => GasUsage.GAS_USAGE_DILUENT,
+    dc.dc_usage_t.DC_USAGE_SIDEMOUNT => GasUsage.GAS_USAGE_SIDEMOUNT,
   };
 }
 
 /// Convert libdivecomputer tank volume type to protobuf TankVolumeType.
-TankVolumeType? _convertTankVolumeType(int dcValue) {
+TankVolumeType _convertTankVolumeType(dc.dc_tankvolume_t dcValue) {
   return switch (dcValue) {
-    0 => TankVolumeType.TANK_VOLUME_TYPE_NONE,
-    1 => TankVolumeType.TANK_VOLUME_TYPE_METRIC,
-    2 => TankVolumeType.TANK_VOLUME_TYPE_IMPERIAL,
-    _ => null,
+    dc.dc_tankvolume_t.DC_TANKVOLUME_NONE => TankVolumeType.TANK_VOLUME_TYPE_NONE,
+    dc.dc_tankvolume_t.DC_TANKVOLUME_METRIC => TankVolumeType.TANK_VOLUME_TYPE_METRIC,
+    dc.dc_tankvolume_t.DC_TANKVOLUME_IMPERIAL => TankVolumeType.TANK_VOLUME_TYPE_IMPERIAL,
   };
 }
 
 /// Convert libdivecomputer deco model type to protobuf DecoModelType.
-DecoModelType? _convertDecoModelType(int dcValue) {
+DecoModelType _convertDecoModelType(dc.dc_decomodel_type_t dcValue) {
   return switch (dcValue) {
-    0 => DecoModelType.DECO_MODEL_TYPE_NONE,
-    1 => DecoModelType.DECO_MODEL_TYPE_BUHLMANN,
-    2 => DecoModelType.DECO_MODEL_TYPE_VPM,
-    3 => DecoModelType.DECO_MODEL_TYPE_RGBM,
-    4 => DecoModelType.DECO_MODEL_TYPE_DCIEM,
-    _ => null,
+    dc.dc_decomodel_type_t.DC_DECOMODEL_NONE => DecoModelType.DECO_MODEL_TYPE_NONE,
+    dc.dc_decomodel_type_t.DC_DECOMODEL_BUHLMANN => DecoModelType.DECO_MODEL_TYPE_BUHLMANN,
+    dc.dc_decomodel_type_t.DC_DECOMODEL_VPM => DecoModelType.DECO_MODEL_TYPE_VPM,
+    dc.dc_decomodel_type_t.DC_DECOMODEL_RGBM => DecoModelType.DECO_MODEL_TYPE_RGBM,
+    dc.dc_decomodel_type_t.DC_DECOMODEL_DCIEM => DecoModelType.DECO_MODEL_TYPE_DCIEM,
   };
 }
 
 /// Convert libdivecomputer deco stop type to protobuf DecoStopType.
-DecoStopType? _convertDecoStopType(int dcValue) {
+DecoStopType _convertDecoStopType(dc.dc_deco_type_t dcValue) {
   return switch (dcValue) {
-    0 => DecoStopType.DECO_STOP_TYPE_NDL,
-    1 => DecoStopType.DECO_STOP_TYPE_SAFETY_STOP,
-    2 => DecoStopType.DECO_STOP_TYPE_DECO_STOP,
-    3 => DecoStopType.DECO_STOP_TYPE_DEEP_STOP,
-    _ => null,
+    dc.dc_deco_type_t.DC_DECO_NDL => DecoStopType.DECO_STOP_TYPE_NDL,
+    dc.dc_deco_type_t.DC_DECO_SAFETYSTOP => DecoStopType.DECO_STOP_TYPE_SAFETY_STOP,
+    dc.dc_deco_type_t.DC_DECO_DECOSTOP => DecoStopType.DECO_STOP_TYPE_DECO_STOP,
+    dc.dc_deco_type_t.DC_DECO_DEEPSTOP => DecoStopType.DECO_STOP_TYPE_DEEP_STOP,
   };
 }
 
 /// Convert libdivecomputer sample event type to protobuf SampleEventType.
-SampleEventType _convertSampleEventType(int dcValue) {
+SampleEventType _convertSampleEventType(dc.parser_sample_event_t dcValue) {
   return switch (dcValue) {
-    0 => SampleEventType.SAMPLE_EVENT_TYPE_NONE,
-    1 => SampleEventType.SAMPLE_EVENT_TYPE_DECO_STOP,
-    2 => SampleEventType.SAMPLE_EVENT_TYPE_RBT,
-    3 => SampleEventType.SAMPLE_EVENT_TYPE_ASCENT,
-    4 => SampleEventType.SAMPLE_EVENT_TYPE_CEILING,
-    5 => SampleEventType.SAMPLE_EVENT_TYPE_WORKLOAD,
-    6 => SampleEventType.SAMPLE_EVENT_TYPE_TRANSMITTER,
-    7 => SampleEventType.SAMPLE_EVENT_TYPE_VIOLATION,
-    8 => SampleEventType.SAMPLE_EVENT_TYPE_BOOKMARK,
-    9 => SampleEventType.SAMPLE_EVENT_TYPE_SURFACE,
-    10 => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP,
-    11 => SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE,
-    12 => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP_VOLUNTARY,
-    13 => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP_MANDATORY,
-    14 => SampleEventType.SAMPLE_EVENT_TYPE_DEEP_STOP,
-    15 => SampleEventType.SAMPLE_EVENT_TYPE_CEILING_SAFETY_STOP,
-    16 => SampleEventType.SAMPLE_EVENT_TYPE_FLOOR,
-    17 => SampleEventType.SAMPLE_EVENT_TYPE_DIVE_TIME,
-    18 => SampleEventType.SAMPLE_EVENT_TYPE_MAX_DEPTH,
-    19 => SampleEventType.SAMPLE_EVENT_TYPE_OLF,
-    20 => SampleEventType.SAMPLE_EVENT_TYPE_PO2,
-    21 => SampleEventType.SAMPLE_EVENT_TYPE_AIR_TIME,
-    22 => SampleEventType.SAMPLE_EVENT_TYPE_RGBM,
-    23 => SampleEventType.SAMPLE_EVENT_TYPE_HEADING,
-    24 => SampleEventType.SAMPLE_EVENT_TYPE_TISSUE_LEVEL,
-    25 => SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE_2,
-    _ => SampleEventType.SAMPLE_EVENT_TYPE_NONE,
+    dc.parser_sample_event_t.SAMPLE_EVENT_NONE => SampleEventType.SAMPLE_EVENT_TYPE_NONE,
+    dc.parser_sample_event_t.SAMPLE_EVENT_DECOSTOP => SampleEventType.SAMPLE_EVENT_TYPE_DECO_STOP,
+    dc.parser_sample_event_t.SAMPLE_EVENT_RBT => SampleEventType.SAMPLE_EVENT_TYPE_RBT,
+    dc.parser_sample_event_t.SAMPLE_EVENT_ASCENT => SampleEventType.SAMPLE_EVENT_TYPE_ASCENT,
+    dc.parser_sample_event_t.SAMPLE_EVENT_CEILING => SampleEventType.SAMPLE_EVENT_TYPE_CEILING,
+    dc.parser_sample_event_t.SAMPLE_EVENT_WORKLOAD => SampleEventType.SAMPLE_EVENT_TYPE_WORKLOAD,
+    dc.parser_sample_event_t.SAMPLE_EVENT_TRANSMITTER => SampleEventType.SAMPLE_EVENT_TYPE_TRANSMITTER,
+    dc.parser_sample_event_t.SAMPLE_EVENT_VIOLATION => SampleEventType.SAMPLE_EVENT_TYPE_VIOLATION,
+    dc.parser_sample_event_t.SAMPLE_EVENT_BOOKMARK => SampleEventType.SAMPLE_EVENT_TYPE_BOOKMARK,
+    dc.parser_sample_event_t.SAMPLE_EVENT_SURFACE => SampleEventType.SAMPLE_EVENT_TYPE_SURFACE,
+    dc.parser_sample_event_t.SAMPLE_EVENT_SAFETYSTOP => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP,
+    dc.parser_sample_event_t.SAMPLE_EVENT_GASCHANGE => SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE,
+    dc.parser_sample_event_t.SAMPLE_EVENT_SAFETYSTOP_VOLUNTARY => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP_VOLUNTARY,
+    dc.parser_sample_event_t.SAMPLE_EVENT_SAFETYSTOP_MANDATORY => SampleEventType.SAMPLE_EVENT_TYPE_SAFETY_STOP_MANDATORY,
+    dc.parser_sample_event_t.SAMPLE_EVENT_DEEPSTOP => SampleEventType.SAMPLE_EVENT_TYPE_DEEP_STOP,
+    dc.parser_sample_event_t.SAMPLE_EVENT_CEILING_SAFETYSTOP => SampleEventType.SAMPLE_EVENT_TYPE_CEILING_SAFETY_STOP,
+    dc.parser_sample_event_t.SAMPLE_EVENT_FLOOR => SampleEventType.SAMPLE_EVENT_TYPE_FLOOR,
+    dc.parser_sample_event_t.SAMPLE_EVENT_DIVETIME => SampleEventType.SAMPLE_EVENT_TYPE_DIVE_TIME,
+    dc.parser_sample_event_t.SAMPLE_EVENT_MAXDEPTH => SampleEventType.SAMPLE_EVENT_TYPE_MAX_DEPTH,
+    dc.parser_sample_event_t.SAMPLE_EVENT_OLF => SampleEventType.SAMPLE_EVENT_TYPE_OLF,
+    dc.parser_sample_event_t.SAMPLE_EVENT_PO2 => SampleEventType.SAMPLE_EVENT_TYPE_PO2,
+    dc.parser_sample_event_t.SAMPLE_EVENT_AIRTIME => SampleEventType.SAMPLE_EVENT_TYPE_AIR_TIME,
+    dc.parser_sample_event_t.SAMPLE_EVENT_RGBM => SampleEventType.SAMPLE_EVENT_TYPE_RGBM,
+    dc.parser_sample_event_t.SAMPLE_EVENT_HEADING => SampleEventType.SAMPLE_EVENT_TYPE_HEADING,
+    dc.parser_sample_event_t.SAMPLE_EVENT_TISSUELEVEL => SampleEventType.SAMPLE_EVENT_TYPE_TISSUE_LEVEL,
+    dc.parser_sample_event_t.SAMPLE_EVENT_GASCHANGE2 => SampleEventType.SAMPLE_EVENT_TYPE_GAS_CHANGE_2,
   };
 }
